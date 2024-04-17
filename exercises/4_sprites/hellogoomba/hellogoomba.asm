fdec930d0fff9111864ba1cf479bac3f61c57963
Paul Yacobellis <pyacobellis@hotmail.com>
2024-04-17 22:26:02 +1000

    goomba sprites!

---
+++ exercises/4_sprites/hellogoomba/hellogoomba.asm
@@ -0,0 +1,167 @@
+.include "consts.inc"
+.include "header.inc"
+.include "reset.inc"
+.include "utils.inc"
+
+.segment "ZEROPAGE"
+Frame:   .res 1              ; Reserve 1 byte to store the number of frames
+Clock60: .res 1              ; Reserve 1 byte to store a counter that increments every second (60 frames)
+BgPtr:   .res 2              ; Reserve 2 bytes (16 bits) to store a pointer to the background address (lo-byte, hi-byte)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PRG-ROM code located at $8000
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.segment "CODE"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Subroutine to load all 32 color palette values from ROM
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.proc LoadPalette
+    PPU_SETADDR $3F00
+    ldy #0                   ; Y = 0
+:   lda PaletteData,y        ; Lookup byte in ROM
+    sta PPU_DATA             ; Set value to send to PPU_DATA
+    iny                      ; Y++
+    cpy #32                  ; Is Y equal to 32?
+    bne :-                   ; Not yet, keep looping
+    rts                      ; Return from subroutine
+.endproc
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Subroutine to load bytes into OAM-RAM starting at $0200
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.proc LoadSprites
+    ldx #0
+LoopSprite:
+    lda SpriteData,x         ; We fetch bytes from the SpriteData lookup table
+    sta $0200,x              ; We store the bytes starting at OAM address $0200
+    inx                      ; X++
+    cpx #16
+    bne LoopSprite           ; Loop 16 times (4 hardware sprites, 4 bytes each)
+    rts
+.endproc
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Subroutine to load tiles and attributes into the first nametable
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.proc LoadBackground
+    lda #<BackgroundData     ; Fetch the lo-byte of BackgroundData address
+    sta BgPtr
+    lda #>BackgroundData     ; Fetch the hi-byte of BackgroundData address
+    sta BgPtr+1
+
+    PPU_SETADDR $2000
+
+    ldx #$00                 ; X = 0 --> x is the outer loop index (hi-byte) from $0 to $4
+    ldy #$00                 ; Y = 0 --> y is the inner loop index (lo-byte) from $0 to $FF
+
+OuterLoop:
+InnerLoop:
+    lda (BgPtr),y            ; Fetch the value *pointed* by BgPtr + Y
+    sta PPU_DATA             ; Store in the PPU data
+    iny                      ; Y++
+    cpy #0                   ; If Y == 0 (wrapped around 256 times)?
+    beq IncreaseHiByte       ;   Then: we need to increase the hi-byte
+    jmp InnerLoop            ;   Else: Continue with the inner loop
+IncreaseHiByte:
+    inc BgPtr+1              ; We increment the hi-byte pointer to point to the next background section (next 255-chunk)
+    inx                      ; X++
+    cpx #4                   ; Compare X with #4
+    bne OuterLoop            ;   If X is still not 4, then we keep looping back to the outer loop
+
+    rts                      ; Return from subroutine
+.endproc
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Reset handler (called when the NES resets or powers on)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+Reset:
+    INIT_NES                 ; Macro to initialize the NES to a known state
+
+    lda #0
+    sta Frame                ; Frame = 0
+    sta Clock60              ; Clock60 = 0
+
+Main:
+    jsr LoadPalette          ; Call LoadPalette subroutine to load 32 colors into our palette
+    jsr LoadBackground       ; Call LoadBackground subroutine to load a full nametable of tiles and attributes
+    jsr LoadSprites          ; Call LoadSprites subroutine to load all sprites into OAM-RAM
+
+EnablePPURendering:
+    lda #%10010000           ; Enable NMI and set background to use the 2nd pattern table (at $1000)
+    sta PPU_CTRL
+    lda #0
+    sta PPU_SCROLL           ; Disable scroll in X
+    sta PPU_SCROLL           ; Disable scroll in Y
+    lda #%00011110
+    sta PPU_MASK             ; Set PPU_MASK bits to render the background
+
+LoopForever:
+    jmp LoopForever          ; Force an infinite execution loop
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; NMI interrupt handler
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+NMI:
+    inc Frame                ; Frame++
+
+    lda #$02                 ; Every frame, we copy spite data starting at $02**.
+    sta $4014                ; The OAM DMA copy starts when we write to $4014.
+
+    lda Frame                ; Increment Clock60 every time we reach 60 frames (NTSC = 60Hz)
+    cmp #60                  ; Is Frame equal to #60?
+    bne :+                   ; If not, bypass Clock60 increment
+    inc Clock60              ; But if it is 60, then increment Clock60 and zero Frame counter
+    lda #0
+    sta Frame
+:
+    rti                      ; Return from interrupt
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; IRQ interrupt handler
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+IRQ:
+    rti                      ; Return from interrupt
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Hardcoded list of color values in ROM to be loaded by the PPU
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+PaletteData:
+.byte $22,$29,$1A,$0F, $22,$36,$17,$0F, $22,$30,$21,$0F, $22,$27,$17,$0F ; Background palette
+.byte $22,$16,$27,$18, $22,$1A,$30,$27, $22,$16,$30,$27, $22,$0F,$36,$17 ; Sprite palette
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Background data (including tiles and attributes, totalling 1KB)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+BackgroundData:
+.incbin "background.nam"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; This is the OAM sprite attribute data data we will use in our game.
+;; We have only one big metasprite that is composed of 4 hardware sprites.
+;; The OAM is organized in sets of 4 bytes per tile.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+SpriteData:
+;      Y   tile#  attributes   X
+.byte $AE,  $3A,  %00000000,  $98
+.byte $AE,  $37,  %00000000,  $A0
+.byte $B6,  $4F,  %00000000,  $98
+.byte $B6,  $4F,  %01000000,  $A0
+
+;;--------------------------------
+;; TODO: Load Goomba sprite
+;;--------------------------------
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Here we add the CHR-ROM data, included from an external .CHR file
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.segment "CHARS"
+.incbin "mario.chr"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Vectors with the addresses of the handlers that we always add at $FFFA
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.segment "VECTORS"
+.word NMI                    ; Address (2 bytes) of the NMI handler
+.word Reset                  ; Address (2 bytes) of the Reset handler
+.word IRQ                    ; Address (2 bytes) of the IRQ handler
